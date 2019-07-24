1e2198a569690b4242224293257f6f913d80343f
ZAHIDUL AMIN <zahidford@gmail.com>
2019-07-23 18:43:31 -0700

    Update Shift_Register_Tb.vhd

--- Shift_Register_Tb.vhd
+++ Shift_Register_Tb.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_std.all;
 
 entity Shift_Register_Test is
-end;
+end Shift_Register_Test;
 		
 --architecture
 
@@ -21,32 +21,29 @@ port (
 		
 end component;
 
-signal	    Reg_A :  std_logic;
+signal	        Reg_A :  std_logic;
 signal		Reg_B :  std_logic;
 signal		Reg_C :  std_logic;
 signal		Reg_D :  std_logic;
 signal		Data_in :std_logic :='0';
 signal		Reset :  std_logic :='0';
-signal		Clock :  std_logic) :='1';
+signal		Clock :  std_logic :='1';
 
 
 begin
-
-	
-	
      dev_to_test : Shift_Register port map (Reg_A,Reg_B,Reg_C,Reg_D,Data_in,Reset,Clock);
-     
      clk_stimulus: process 
 		begin
 		wait for 10 ns;
 		Clock <= not Clock;
-	 end process clk_stimulus;
+     end process clk_stimulus;
 	 
-	 data_stimulus: process
+     data_stimulus: process
 	 	begin
 	 	wait for 10 ns;
 	 	Data_In <= not Data_in;
 	 	wait for 150 ns;
-	 end process data_stimulus; 
+      end process data_stimulus; 
+     
 
 end test;
