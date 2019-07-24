8b70067bf94862214439c7dfa209a81bd0c813a6
ZAHIDUL AMIN <zahidford@gmail.com>
2019-07-23 18:42:43 -0700

    Update Shift_Register.vhd

--- Shift_Register.vhd
+++ Shift_Register.vhd
@@ -1,6 +1,7 @@
-library IEEE;
+
+Library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_std.all;
+use IEEE.NUMERIC_std.All;
 
 entity Shift_Register is
 
@@ -11,15 +12,14 @@ port (
 		Reg_D : out std_logic;
 		Data_in : in std_logic;
 		Reset : in std_logic;
-		Clock : in std_logic);
+		Clock : in std_logic
+);
 end Shift_Register;		
---architecture
+--architecture;
 
 architecture behaviour of Shift_Register is
-
 signal A_Reg, B_Reg : std_logic :='0';
 signal C_Reg, D_Reg : std_logic :='0';
-
 
 begin
 
@@ -30,8 +30,8 @@ Reg_C <=C_Reg;
 Reg_D <=D_Reg;
 	
 reg_process: process(Clock) 
-begin
-	if(rising_edge(Clock) then
+	begin
+	if(rising_edge(Clock)) then
 		 if (Reset = '1') then
 			A_Reg <='0';
 			B_Reg <='0';
@@ -43,7 +43,10 @@ begin
 			C_Reg <= B_Reg;
 			D_Reg <= C_Reg;
 		 end if;
-	end if
+	end if;
 	end process reg_process;
+
+
+
 
 end behaviour;
